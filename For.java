87669c7dfc8d617e30d08d32de2b7e8600153729
Kuangjugaza <yoon2449@naver.com>
2020-07-19 19:46:56 +0800

    Java Java

---
+++ For.java
@@ -0,0 +1,20 @@
+package coco1;
+
+public class For {
+
+	public static void main(String[] args) {
+
+		
+		int num;
+		int sum = 0;
+		t
+		for(num=1; num<=10; num++) {
+				sum += num;
+				
+		}
+		System.out.println(sum);
+
+	}
+
+
+}
